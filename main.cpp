3878c549d7fde968b8817fcc0bb364090a5cda19
Memo <gmanriquev@unsa.edu.pe>
2026-01-21 19:11:56 -0500

    primer commit de la rama base

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+class Person {
+  public:
+  string name;
+  int age;
+  Person (string, int);
+};
 
 int main (int argc, char *argv[]) {
   cout << "Hello world";
   return 0;
 }
+
+
+
+Person::Person (string name , int age): name(name), age(age) { }
